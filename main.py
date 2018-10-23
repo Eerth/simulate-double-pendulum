be61d361bdccb76e9d0b2cc4e10d702e50dc60a4
Eert <eert@live.nl>
2018-10-23 15:30:02 +0200

    use new model for pendulum

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ Created on Fri Oct 12 00:07:05 2018
 
 @author: eert
 
-State: y = [q1, q2, q1d, q2d]
+States y = [q1, q2, qd1, qd2]'
+Control u: Torque on first joint
 """
 
 import numpy as np 
@@ -15,54 +16,73 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
 # Constants
-m1 = 1
-m2 = 1
-l1 = 1
-l2 = 1
-g = 9.81
+#m1 = 1
+#m2 = 1
+#l1 = 1
+#l2 = 1
+#g = 9.81
 
-T = 10
+# Parameters
+par = {'g':  9.81,  #[m/s²]
+       'l1': 0.2,   #[m]
+       'l2': 0.2,   #[m]
+       'm1': 0.2,   #[kg]
+       'm2': 0.1,   #[m]
+       'c1':-0.06,  #[m]
+       'c2': 0.08,  #[m]
+       'I1': 0.09,  #[kgm^2]
+       'I2': 0.0002,#[kgm^2]
+       'b1': 5.,    #[kgs^-1]
+       'b2': 0.0003,#[kgs^-1]
+       'km': 60.,    #[Nm]
+       'te': 0.05}  #[s]
+
+T = 10.
 nDisControls = 5
-u_dis = [0, 3, 1, 2, 5]
-t_dis = np.linspace(0, T, nDisControls)
+u_dis = [0., 3., 1., 2., 5.]
+t_dis = np.linspace(0., T, nDisControls)
 
 def der_fun(t, y):
     #u = np.interp(t_dis, u_dis, t)
+    u = 0.
     
     q1 = y[0]
     q2 = y[1]
-    q1d = y[2]
-    q2d = y[3]
+    qd1 = y[2]
+    qd2 = y[3]
     
-    delta = q2 - q1
+    P1 = par['m1']*par['c1']**2 + par['m2']*par['l1']**2 + par['I1']
+    P2 = par['m2']*par['c2']**2 + par['I2']
+    P3 = par['m2']*par['l1']*par['c2']
     
-    q1dd = ( m2 * l1 * q1d * q1d * sin(delta) * cos(delta) \
-         + m2 *g * sin(q2) * cos(delta) \
-         + m2 * l2 * q2d * q2d * sin(delta) \
-         - (m1 + m2 ) * g *sin(q1) ) / ( (m1 + m2 ) * l1 \
-         - m2 * l1 * cos(delta) * cos(delta) )
-    q2dd = ( - m2 * l2 * q2d * q2d  * sin(delta) * cos(delta) \
-         + ( m1 + m2 ) * (g * sin(q1) * cos(delta) \
-         - l1 * q1d * q1d * sin(delta) \
-         - g * sin(q2))) / ( (m1 + m2 ) * l2 \
-         - m2 * l2 * cos(delta) * cos(delta) )
+    g1 =(par['m1']*par['c1'] + par['m2']*par['l1'])*par['g']
+    g2 = par['m2']*par['c2']*par['g']
     
-    return [q1d, q2d, q1dd, q2dd]
+    M = np.array([[P1 + P2 + 2*P3*cos(q2), P2 + P3*cos(q2)], [P2 + P3*cos(q2), P2]])
+    
+    C = np.array([[par['b1'] - P3*qd2*sin(q2), -P3*(qd1+qd2)*sin(q2)], [P3*qd1*sin(q2), par['b2']]])
+ 
+    G = np.array([[-g1*sin(q1) - g2*sin(q1+q2)], [-g2*sin(q1 + q2)]])
+ 
+    qdd = np.linalg.solve(M, np.array([[u, 0]]).T - C @ np.array([[qd1, qd2]]).T - G)
+    
+    return [qd1, qd2, qdd[0], qdd[1]]
 
-y0 = [pi/2, pi/2, 0, 0]
-
+# Solve ODE
+y0 = [pi, pi/2, 0, 0]
 sol = integrate.solve_ivp(der_fun, [0, 10], y0)
 
 # Positions
-x1 = l1*sin(sol.y[0,:])
-y1 = -l1*cos(sol.y[0,:])
-x2 = l2*sin(sol.y[1,:]) + x1
-y2 = -l2*cos(sol.y[1,:]) + y1
+x1 = -par['l1'] * sin(sol.y[0,:])
+y1 =  par['l1'] * cos(sol.y[0,:])
+x2 = -par['l2'] * sin(sol.y[1,:]) + x1
+y2 =  par['l2'] * cos(sol.y[1,:]) + y1
 
 # Plot
 fig = plt.figure()
 ax = fig.add_subplot(111, aspect='equal', autoscale_on=False,
-                     xlim=(min(min(x1), min(x2)), max(max(x1), max(x2))), ylim=(min(min(y1), min(y2)), max(max(y1), max(y2))))
+                     xlim=(min(min(x1), min(x2)), max(max(x1), max(x2))), 
+                     ylim=(min(min(y1), min(y2)), max(max(y1), max(y2))))
 ax.grid()
 line, = ax.plot([], [], 'o-', lw=2)
 
